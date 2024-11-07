84bb9438bcc6bea912576bdee6893a43c653800b
Arseniy Merzlyakov <merzliakov.ad@phystech.edu>
2024-11-07 19:56:46 +0300

    add end_to_end tests

---
+++ tests/check_end_to_end.py
@@ -0,0 +1,30 @@
+from subprocess import run, Popen, PIPE
+from sys import executable
+
+num_test = 1
+is_ok = True
+for i in range(1, 6):
+    str_data =  "tests/end_to_end/" + str(i) + ".dat"
+    file_in = open(str_data, "r")
+    str_ans = "tests/end_to_end/" + str(i) + ".dat.ans"
+
+    ans = []
+    for i in open(str_ans):
+        ans.append(float(i.strip()))
+	
+    result = run(["build/src/main"], capture_output = True, encoding='cp866', stdin=file_in)
+    print("Test: " + str(num_test).strip())
+
+    res = list(map(float, result.stdout.split()))
+    is_ok &= (res == ans)
+    if abs(res[0] - ans[0]) <= 0.0000001:
+        print("OK")
+    else:
+        print("ERROR\nExpect:", ans[0], "\nGive:  ", res[0])
+    print("-------------------------------------------------")
+    num_test += 1
+
+if is_ok:
+	print("TESTS PASSED")
+else:
+	print("TESTS FAILED")
